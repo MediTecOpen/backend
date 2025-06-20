0949f15841d75de4cc5a80be88ac5a932afdf72f
UPC-Skylar <zaynml2021@gmail.com>
2025-06-19 23:51:42 -0500

    initial commit

---
+++ src/main/java/com/meditech/careme/platform/CaremePlatformApplication.java
@@ -0,0 +1,11 @@
+package com.meditech.careme.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaremePlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CaremePlatformApplication.class, args);
+    }
+}
